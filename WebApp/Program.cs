958439e0dec62be2b95cb5046733ac57d497e516
Nguyễn Trần Dinh <vinhdtq123123123@gmail.com>
2025-09-26 16:18:32 +0700

    Update and create WebAPI Structure

---
+++ WebApp/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Public/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            // Route cho tất cả Area (Admin, Public,...)
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+            // Route default, nếu truy cập / thì tự động vào Area Public
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}",
+                defaults: new { area = "Public", controller = "Home", action = "Index" });
+
+            app.Run();
+        }
+    }
+}
