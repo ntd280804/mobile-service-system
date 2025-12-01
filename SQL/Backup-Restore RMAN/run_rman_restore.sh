c79bce51c3c49f4884c47ad9b1651647c9e9e10d
Nguyễn Trần Dinh <vinhdtq123123123@gmail.com>
2025-12-01 22:46:03 +0700

    update

--- SQL/Backup-Restore RMAN/run_rman_restore.sh
+++ SQL/Backup-Restore RMAN/run_rman_restore.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
+# -------------------------------
+# Script RMAN Restore PDB
+# -------------------------------
+
 export ORACLE_HOME=/opt/oracle/product/21c/dbhome_1
 export PATH=$ORACLE_HOME/bin:$PATH
 export ORACLE_SID=ORCLCDB
 
 PDB_SERVICE=ORCLPDB1
 POINTOFTIME="$1"
-
-if [ -z "$POINTOFTIME" ]; then
-    echo "❌ Missing Point-In-Time value."
-    echo "Usage: $0 \"YYYY-MM-DD HH24:MI:SS\""
-    exit 1
-fi
 
 LOG_DIR="/home/oracle/backup_web/logs"
 mkdir -p "${LOG_DIR}"
@@ -22,21 +20,41 @@ sqlplus -s / as sysdba <<EOF
 ALTER PLUGGABLE DATABASE ${PDB_SERVICE} CLOSE IMMEDIATE;
 EOF
 
-echo "===> Running RMAN PITR restore..."
-rman target / <<EOF >> ${LOG_FILE} 2>&1
+echo "===> Running RMAN restore..."
+if [ -z "$POINTOFTIME" ]; then
+    # Restore latest backup
+    rman target / <<EOF >> ${LOG_FILE} 2>&1
+RUN {
+  RESTORE PLUGGABLE DATABASE ${PDB_SERVICE};
+  RECOVER PLUGGABLE DATABASE ${PDB_SERVICE};
+}
+EOF
+
+    echo "===> Opening PDB ${PDB_SERVICE} (no RESETLOGS)..."
+    sqlplus -s / as sysdba <<EOF
+ALTER PLUGGABLE DATABASE ${PDB_SERVICE} OPEN;
+EOF
+else
+    # PITR restore
+    rman target / <<EOF >> ${LOG_FILE} 2>&1
 RUN {
   SET UNTIL TIME "to_date('${POINTOFTIME}', 'YYYY-MM-DD HH24:MI:SS')";
   RESTORE PLUGGABLE DATABASE ${PDB_SERVICE};
   RECOVER PLUGGABLE DATABASE ${PDB_SERVICE};
-  ALTER PLUGGABLE DATABASE ${PDB_SERVICE} OPEN RESETLOGS;
 }
 EOF
+
+    echo "===> Opening PDB ${PDB_SERVICE} with RESETLOGS..."
+    sqlplus -s / as sysdba <<EOF
+ALTER PLUGGABLE DATABASE ${PDB_SERVICE} OPEN RESETLOGS;
+EOF
+fi
 
 RET=$?
 if [ $RET -ne 0 ]; then
   echo "❌ RESTORE FAILED — check log: ${LOG_FILE}"
 else
-  echo "✅ RESTORE SUCCESS (PITR) — log: ${LOG_FILE}"
+  echo "✅ RESTORE SUCCESS — log: ${LOG_FILE}"
 fi
 
 exit $RET
